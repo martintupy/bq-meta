8b8420a0117b245fe6a12f20d068f8b78784e2fa
Martin Tupy <martin.tupy@kiwi.com>
2023-10-25 14:05:34 +0200

    chore: remove tag-dev.sh

--- bin/tag-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
-VERSION=$(cat setup.cfg | grep "version =" | cut -d = -f 2 | xargs)
-TIMESTAMP=$(python -c 'from datetime import datetime; print(datetime.utcnow().strftime("%Y%m%d%H%M%S"))')
-git tag $VERSION+$TIMESTAMP -a -m "Version: $VERSION"
-git push origin --tags
